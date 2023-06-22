8f3df67612e0003defa1f163db6605296fc0649b
metehandoenmez <metehandoenmez@gmail.com>
2023-06-22 15:06:03 +0300

    completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import "./App.css"
 
-import {Routes, Route, Link} from "react-router-dom"
-import Counter from "./counter"
+import { Routes, Route, Link } from "react-router-dom";
+import Counter from "./counter";
 import InteractiveWelcome from "./InteractiveWelcome";
-import ShowGithubUser from "./ShowGithubUser"
+import ShowGithubUser from "./ShowGithubUser";
+import GithubUserList from "./GithubUserList"; // Import the GithubUserList component
 
 export default function App() {
-
- 
-    return (
-      
-        <div style={{display:"flex"}}>
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<InteractiveWelcome />}></Route>
-              <Route path="/counter" element={<Counter></Counter>}></Route>
-              <Route path="/users/:username" element={<ShowGithubUser></ShowGithubUser>}></Route>
-              <Route path="/*" element={<div><h1>404</h1><h2>Not Found</h2></div>}></Route>
-            </Routes>
-          </div>
-      
-        <div className="container"> 
-          <p>Links</p>
-          <Link to={"/"}>Homepage</Link>
-          <Link to={"/counter"}>Counter</Link>
-          <Link to={"/users/metehandoenmez"}>My Github Profile</Link>
-          <Link to={"/users/github"}>Github's Github Profile</Link>
-        </div>
+  return (
+    <div style={{ display: "flex" }}>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<InteractiveWelcome />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="users" element={<GithubUserList />}>
+            <Route path=":username" element={<ShowGithubUser/>}></Route>
+          </Route>
+          <Route path="/*" element={<div><h1>404</h1><h2>Not Found</h2></div>} />
+        </Routes>
       </div>
-    )
-  }
+      <div className="container">
+        <p>Links</p>
+        <Link to={"/"}>Homepage</Link>
+        <Link to={"/counter"}>Counter</Link>
+        <Link to={"/users"}>GitHub Users</Link>
+      </div>
+    </div>
+  );
+}
--- src/GithubUserList.jsx
+++ src/GithubUserList.jsx
@@ -1,45 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import GithubUser from "./GithubUser.jsx";
-
-
+import { Link, Route, Routes, Outlet } from "react-router-dom";
+import ShowGithubUser from "./ShowGithubUser";
 
 export default function GithubUserList() {
- 
-    const [list, setList] = useState([])
-    const [input, setInput] = useState("")
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
 
-    const handleInput = (event) => {
-        setInput(event.target.value)
-    }
- 
-    const handleList = () => {
-        setList((arr) => [...arr, input]);
-        setInput("")
-    }
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
 
+  const handleList = () => {
+    setList((arr) => [...arr, input]);
+    setInput("");
+  };
+
+  return (
     
-    
+    <div style={{ display: "flex", flexDirection: "row" }}>
 
-    return (
-        <div style={{display:"flex", flexDirection:"row"}}>
-            <div className="container">
-                <input
-                onChange={handleInput}
-                placeholder="Enter a GitHub username"
-                value={input}
-                />
-                <button onClick={handleList}>Add User</button>
-                <button onClick={() => {
-                    setList([])
-                }}>Clear</button>
-            </div>
-            <div style={{display:"flex", flexDirection:"row"}} >
-                {list.map((user) => (
-                    <GithubUser username={user} key={user + "key"}/>
-                ))}
-            </div>
-        </div>
-    )
-                    
+      <div className="container">
+        <input
+          onChange={handleInput}
+          placeholder="Enter a GitHub username"
+          value={input}
+        />
+        <button onClick={handleList}>Add User</button>
+        <button
+          onClick={() => {
+            setList([]);
+          }}
+        >
+          Clear
+        </button>
+      </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {list.map((user) => (
+          <div style={{padding:"10px"}}>
+              <div>
+                  <Link
+                    key={user + Math.floor(Math.random() * 15)}
+                    to={`/users/${user}`}
+                    style={{ marginRight: "10px" }}
+                  >
+                    {user}
+                  </Link>
+              </div>
+          </div>
+        ))}
+        <br />
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 }
