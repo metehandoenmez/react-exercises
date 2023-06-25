34082582a5916f9e60ff71b3088c44b536d07476
metehandoenmez <metehandoenmez@gmail.com>
2023-06-25 14:45:16 +0300

    completed exercise

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,45 +1,78 @@
-import React, { useState, useEffect } from "react";
-import "./App.css"
+import React from "react";
+import useSWR from "swr";
+import "./App.css";
 
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export default function GithubUser({username}) {
- 
-    const [dataObj, setDataObj] = useState([])
-    
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response =>
-            response.json())
-            .then(data => 
-                setDataObj(data))
-    }, [username])
-    
-    const imgStyle = {
-        borderRadius: "50%",
-        padding: "10px",
-        width:"180px"
-    }
+const useGithubUser = (username) => {
+  const { data: dataObj, error } = useSWR(`https://api.github.com/users/${username}`, fetcher);
 
+  const loading = !dataObj && !error;
 
+  return { dataObj, error, loading };
+};
 
-                if(dataObj.message === "Not Found") {
-                   return (<div className="container"><h2>User not found.</h2></div>) 
-                }
-                else {
-                    return (
-                        <div>
-                        <img src={dataObj.avatar_url} alt="" style={imgStyle}></img>
-                        <p><h4><u>Username</u></h4> {dataObj.login}</p>
-                        <br />
-                        <p><h4><u>Location</u></h4> {dataObj.location}</p>
-                        <br />
-                        <p><b>Followers</b>: {dataObj.followers}, <b>Following</b>: {dataObj.following}</p>
-                        <br />
-                        <p><h4><u>Public Repositories</u></h4> {dataObj.public_repos}</p>
-                        <br />
-                        <a href={dataObj.html_url}><button>View on GitHub</button></a>
-                    </div>
-                    )
-                    
-                }
+export default function GithubUser({ username }) {
+  const { dataObj, error, loading } = useGithubUser(username);
+
+  const imgStyle = {
+    borderRadius: "50%",
+    padding: "10px",
+    width: "180px",
+  };
+
+  if (loading) {
+    return <div className="container">Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div className="container">
+        <h2>Error: {error.message}</h2>
+      </div>
+    );
+  }
+
+  if (dataObj.message === "Not Found") {
+    return (
+      <div className="container">
+        <h2>User not found.</h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <img src={dataObj.avatar_url} alt="" style={imgStyle}></img>
+        <p>
+          <h4>
+            <u>Username</u>
+          </h4>{" "}
+          {dataObj.login}
+        </p>
+        <br />
+        <p>
+          <h4>
+            <u>Location</u>
+          </h4>{" "}
+          {dataObj.location}
+        </p>
+        <br />
+        <p>
+          <b>Followers</b>: {dataObj.followers}, <b>Following</b>:{" "}
+          {dataObj.following}
+        </p>
+        <br />
+        <p>
+          <h4>
+            <u>Public Repositories</u>
+          </h4>{" "}
+          {dataObj.public_repos}
+        </p>
+        <br />
+        <a href={dataObj.html_url}>
+          <button>View on GitHub</button>
+        </a>
+      </div>
+    );
+  }
 }
